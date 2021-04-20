aa1db6a2da8d8cd179f255edf8d0e400181db5f8
amitkrmaharana <ak657harley@gmail.com>
2021-04-20 21:58:00 +0530

    Refactored: Calculated Daily wage if Employee is present

--- employeeWage.py
+++ employeeWage.py
@@ -1,15 +1,18 @@
 import random
 
 isAbsent = 0
+WAGE_PER_HOUR = int(20)
+FULL_DAY_HOUR = int(8)
 
 
-def employee_check():
+def employee_wage():
     # Using Random To check if Employee is Absent or Present
     random_check = random.randint(0, 2)
     if random_check == isAbsent:
         print("Employee is Absent")
     else:
-        print("Employee is Present")
+        daily_wage = int(WAGE_PER_HOUR * FULL_DAY_HOUR)
+        print("Employee is Present And Earns Rs.{}".format(daily_wage))
 
 
-employee_check()
+employee_wage()
