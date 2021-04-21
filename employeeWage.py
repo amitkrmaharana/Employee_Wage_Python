b0f16bf12f1b880fc5ab0a1ad2969d13180ada50
amitkrmaharana <ak657harley@gmail.com>
2021-04-21 09:10:38 +0530

    Refactored: calculated wages til either max working days or hours is reached

--- employeeWage.py
+++ employeeWage.py
@@ -19,14 +19,20 @@ def emp_hr_check(emp_hr):
 
 def employee_wage():
     monthly_wage = 0
-    for day in range(0, 20):
+    total_working_hours = 0
+    total_working_days = 0
+    while total_working_days < 20 and total_working_hours < 60:
         # Using Random To check if Employee is Absent or Present
         random_check = random.randint(0, 3)  # random numbers from 0 to 3 excluding 3
         emp_hr = emp_hr_check(random_check)  # returned employee working hour
+        total_working_hours += emp_hr
+        total_working_days += 1
         daily_wage = emp_hr * WAGE_PER_HOUR  # calculated daily employee wage
         monthly_wage += daily_wage
 
     print("Monthly wage for the employee is Rs.", monthly_wage)
+    print("Total number of days worked in a month is ", total_working_days)
+    print("Total number of hours worked in a month is ", total_working_hours)
 
 
 employee_wage()
