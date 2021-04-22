da8a2f161105f9c0cc30352b4e69038d7652f461
amitkrmaharana <ak657harley@gmail.com>
2021-04-22 09:53:27 +0530

    Reafctored: Added daily wages to a list

--- employeeWage.py
+++ employeeWage.py
@@ -17,6 +17,7 @@ class EmpWageBuilder:
         total_working_days = 0
         total_working_hours = 0
         monthly_wage = 0
+        daily_wage_list = []
         while total_working_days < self.max_working_days and total_working_hours < self.max_working_hours:
             # Using Random To check if Employee is Absent or Present
             attendance_check = random.randint(0, 3)  # random numbers from 0 to 3 excluding 3
@@ -24,11 +25,14 @@ class EmpWageBuilder:
             total_working_hours += emp_hr
             total_working_days += 1
             daily_wage = emp_hr * self.wage_per_hour  # calculated daily employee wage
+            daily_wage_list.append(daily_wage)
             monthly_wage += daily_wage
         print("Employee works in ", self.company_name)
         print("Monthly wage for the employee is Rs.", monthly_wage)
         print("Total number of days worked in a month is ", total_working_days)
         print("Total number of hours worked in a month is ", total_working_hours)
+        print("Daily wages are as given below")
+        print(daily_wage_list)
 
     @staticmethod  # static methods are those methods which are bound to a class rather than to its object
     def emp_hr_check(emp_hr):
