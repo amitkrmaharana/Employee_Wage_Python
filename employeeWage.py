49f1c858b161c49074a193111942422efbe0f859
amitkrmaharana <ak657harley@gmail.com>
2021-04-22 09:28:54 +0530

    Refactored: Refactored the file to a class object type to calculate daily wages for multiple companies

--- employeeWage.py
+++ employeeWage.py
@@ -1,38 +1,46 @@
 import random
 
-isAbsent = 0
-isFullTime = 1
-isPartTime = 2
-WAGE_PER_HOUR = 20
-FULL_DAY_HOUR = 8
-PART_TIME_HOUR = 4
+
+class EmpWageBuilder:
+    IS_ABSENT = 0
+    IS_PART_TIME = 1
+    IS_FULL_TIME = 2
+
+    # constructor
+    def __init__(self, company_name, wage_per_hour, max_working_hours, max_working_days):
+        self.company_name = company_name
+        self.wage_per_hour = wage_per_hour
+        self.max_working_hours = max_working_hours
+        self.max_working_days = max_working_days
+
+    def calculate_emp_wage(self):
+        total_working_days = 0
+        total_working_hours = 0
+        monthly_wage = 0
+        while total_working_days < self.max_working_days and total_working_hours < self.max_working_hours:
+            # Using Random To check if Employee is Absent or Present
+            attendance_check = random.randint(0, 3)  # random numbers from 0 to 3 excluding 3
+            emp_hr = EmpWageBuilder.emp_hr_check(attendance_check)  # returned employee working hour
+            total_working_hours += emp_hr
+            total_working_days += 1
+            daily_wage = emp_hr * self.wage_per_hour  # calculated daily employee wage
+            monthly_wage += daily_wage
+        print("Employee works in ", self.company_name)
+        print("Monthly wage for the employee is Rs.", monthly_wage)
+        print("Total number of days worked in a month is ", total_working_days)
+        print("Total number of hours worked in a month is ", total_working_hours)
+
+    @staticmethod  # static methods are those methods which are bound to a class rather than to its object
+    def emp_hr_check(emp_hr):
+        # get_emp_hr is a dictionary data type here
+        get_emp_hr = {
+            EmpWageBuilder.IS_PART_TIME: 4,
+            EmpWageBuilder.IS_FULL_TIME: 8
+        }
+        return get_emp_hr.get(emp_hr, 0)  # returns the employee hour and 0 represents the default value
 
 
-def emp_hr_check(emp_hr):
-    # using switch case statements
-    switcher = {
-        1: FULL_DAY_HOUR,
-        2: PART_TIME_HOUR
-    }
-    return switcher.get(emp_hr, 0)  # returns the employee hour and 0 represents the default value
-
-
-def employee_wage():
-    monthly_wage = 0
-    total_working_hours = 0
-    total_working_days = 0
-    while total_working_days < 20 and total_working_hours < 60:
-        # Using Random To check if Employee is Absent or Present
-        random_check = random.randint(0, 3)  # random numbers from 0 to 3 excluding 3
-        emp_hr = emp_hr_check(random_check)  # returned employee working hour
-        total_working_hours += emp_hr
-        total_working_days += 1
-        daily_wage = emp_hr * WAGE_PER_HOUR  # calculated daily employee wage
-        monthly_wage += daily_wage
-
-    print("Monthly wage for the employee is Rs.", monthly_wage)
-    print("Total number of days worked in a month is ", total_working_days)
-    print("Total number of hours worked in a month is ", total_working_hours)
-
-
-employee_wage()
+employee1 = EmpWageBuilder("Microsoft", 20, 60, 20)
+employee2 = EmpWageBuilder("IBM", 15, 70, 23)
+employee1.calculate_emp_wage()
+employee2.calculate_emp_wage()
